cc1f97f22bac930f57caca605ba6f50870adc6d3
JXB-JUTTBRAND <106441016+JUTTBRAND@users.noreply.github.com>
2023-04-02 10:58:08 +0500

    Update JXB.py

--- JXB.py
+++ JXB.py
@@ -1,3 +1,3 @@
 import AWAIS
-os.system('xdg-open https://facebook.com/groups/302474258349320/')
+#os.system('xdg-open https://facebook.com/groups/302474258349320/')
 print("\033[1;34m CONGRATULATIONS THIS TOOL SPORTED YOUR PHONE ")
